31f310c0a1fce5204234a9d9280224a4c6ab3abe
jusutdoencode <poyraz50_38@hotmail.com>
2021-03-05 22:41:29 +0300

    05.03.2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
 using System;
 
 namespace ConsoleUI
@@ -8,12 +10,9 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            CarManager carManager = new CarManager(new InMemoryCarDal());
+            CarManager carManager = new CarManager(new EfCarDal());
 
-            foreach (var car in carManager.GetAll())
-            {
-                Console.WriteLine(car.DailyPrice);
-            }
+            carManager.Add(new Car { BrandId = 1, ColorId = 5, DailyPrice = 200, Description = "Corollo", Id = 1, ModelYear = 2010 });
         }
     }
 }
